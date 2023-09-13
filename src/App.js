2e055d953b0eb84d86f63ddcc6e6818506491c6d
Ossi Kauppi <ossi.kauppi@gmail.com>
2023-09-13 18:01:38 +0300

    Shpping Demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { FaShoppingCart } from 'react-icons/fa';
+import { useState } from 'react';
 
 function App() {
+  const [cart, setCart] = useState([]);
+  const [products, setProducts] = useState([
+    {name: "Pulla", price: 10},
+    {name: "Porkkana", price: 5},
+    {name: "Tomaatti", price: 3}
+  ]);
+
+  //function osta() {}
+  const osta = (e, index) => {
+    e.preventDefault();
+    const product = products[index];
+    setCart([...cart,product])
+
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <h1>My Shopping Cart</h1>
+      <p><FaShoppingCart />{cart.length}</p>
+      {
+        products.map((product, index) =>(
+          <div id="item" key={index}>
+            <form onSubmit={(e) => osta(e, index)}>
+              <h3>{product.name}</h3>
+              <p>{product.price}€</p>
+              <button>Osta pois</button>
+            </form>
+          </div>
+        ))
+      }
+
     </div>
   );
 }
